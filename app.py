e09222ae3e39d7699593527ed28a8f2bd825e146
David Arena <david@davidarena.com>
2020-12-09 20:38:14 -0500

    Commented app.py

--- app.py
+++ app.py
@@ -37,13 +37,14 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///records.db")
 
-
+# Load the landing page
 @app.route("/", methods=["GET"])
 @login_required
 def index():
     if request.method == "GET":
         return render_template("index.html")
 
+# Load the login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
@@ -84,6 +85,7 @@ def login():
         return render_template("login.html")
 
 
+# Log user out
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -95,10 +97,12 @@ def logout():
     return redirect("/")
 
 
+# Register new user
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
     if request.method == "POST":
+        # Validate inputs of new user
         if not request.form.get("username"):
             flash("Please provide a username.")
             return render_template("register.html")
@@ -129,51 +133,65 @@ def register():
         return render_template("register.html")
 
 
+# Load page where user can select teams
 @app.route("/teams", methods=["GET", "POST"])
 @login_required
 def teams():
     if request.method == "POST":
+        # User did not select team
         if not request.form.get("team"):
             flash("Please select a team.")
             teams = db.execute("SELECT * FROM teams WHERE user_id = ?", session["user_id"])
             return render_template("teams.html", teams=teams)
         
+        # Issue GET request to view_team with the given team_id
         return redirect("/view_team?team_id=" + request.form.get("team"))
 
     elif request.method == "GET":
+        # Get the teams associated with the current user to populate dropdown
         teams = db.execute("SELECT * FROM teams WHERE user_id = ?", session["user_id"])
+        # If user has no associated teams, the template will display a message indicating such
         return render_template("teams.html", teams=teams)
 
 
+# View a team roster
 @app.route("/view_team", methods=["GET"])
 @login_required
 def view_team():
     if request.method == "GET":
+        # Get the team name from the provided team_id, team must also be associated with current user
         name = db.execute("SELECT name FROM teams WHERE user_id = ? AND id = ?", session["user_id"], request.args.get("team_id"))
         if not name:
             return apology("Team not found", 404)
+        # Get player names from team_id
         players = db.execute("SELECT name FROM players WHERE user_id = ? AND team_id = ?", session["user_id"], request.args.get("team_id"))
         return render_template("view_team.html", name=name[0]['name'], players=players)
 
 
+# Create a new team
 @app.route("/add_team", methods=["GET", "POST"])
 @login_required
 def add_team():
     if request.method == "POST":
+        # User did not submit team name
         if not request.form.get("tname"):
             flash("Please enter a team name.")
             return render_template("add_team.html")
 
+        # Get names from form
         names = []
         for i in range(1,9):
             if request.form.get("player" + str(i)):
                 names.append(request.form.get("player" + str(i)))
+        # Team must have at least one player
         if not names:
             flash("Team must have at least one player.")
             return render_template("add_team.html")
 
+        # Create teams
         team_id = db.execute("INSERT INTO teams (user_id, name) VALUES (?, ?)", session["user_id"], request.form.get("tname"))
 
+        # Create players
         for name in names:
             db.execute("INSERT INTO players (user_id, name, team_id) VALUES (?, ?, ?)", session["user_id"], name, team_id)
 
@@ -184,21 +202,27 @@ def add_team():
         return render_template("add_team.html")
     
 
+# Create a new game
 @app.route("/new_game", methods=["GET", "POST"])
 @login_required
 def new_game():
     if request.method == "POST":
+        # User must select a team from each dropdown
         if not request.form.get("left_team") or not request.form.get("right_team"):
             flash("Please select two teams.")
             teams = db.execute("SELECT * FROM teams WHERE user_id = ?", session["user_id"])
             return render_template("new_game.html", teams=teams)
+        # Failsafe in case user somehow selects same team twice
         elif request.form.get("left_team") == request.form.get("right_team"):
             flash("Teams must not be the same.")
             teams = db.execute("SELECT * FROM teams WHERE user_id = ?", session["user_id"])
             return render_template("new_game.html", teams=teams)
 
+        # Create new game
         game_id = db.execute("INSERT INTO games (user_id, left_team_id, right_team_id) VALUES (?, ?, ?)", session["user_id"],
                              request.form.get("left_team"), request.form.get("right_team"))
+
+        # Update session to reflect current game state
         session["game_id"] = game_id
         session["tossup_num"] = 1
 
@@ -208,14 +232,17 @@ def new_game():
         teams = db.execute("SELECT * FROM teams WHERE user_id = ?", session["user_id"])
         return render_template("new_game.html", teams=teams)
 
-    
+
+# View the score entry page for the current game
 @app.route("/game", methods=["GET", "POST"])
 @login_required
 def game():
     if request.method == "POST":
+        # Cannot find session data
         if "game_id" not in session or "tossup_num" not in session:
             return apology("It appears you do not have a game in progress.", 404)
 
+        # Add 10 bonus points for every checkbox selected
         bonus = 0
         if request.form.get("b-checkbox-1"):
             bonus += int(request.form.get("b-checkbox-1"))
@@ -224,10 +251,12 @@ def game():
         if request.form.get("b-checkbox-3"):
             bonus += int(request.form.get("b-checkbox-3"))
 
+        # Error checking
         if request.form.get("scoring-player") and request.form.get("negging-player"):
             if request.form.get("scoring-player") == request.form.get("negging-player"):
                 return apology("Scoring player and negging player cannot be the same", 400)
 
+        # Create a `score_event` for the scoring player, recording their player_id, team_id, tossup #, score, and bonus points
         if request.form.get("score"):
             team_id = db.execute("SELECT team_id FROM players WHERE id = ?", request.form.get("scoring-player"))
             if not team_id:
@@ -236,6 +265,8 @@ def game():
                 team_id = team_id[0]["team_id"]
             db.execute("INSERT INTO score_events (game_id, team_id, player_id, tossup_num, score, bonus) VALUES (?, ?, ?, ?, ?, ?)", 
                        session["game_id"], team_id, request.form.get("scoring-player"), session["tossup_num"], request.form.get("score"), bonus)
+
+        # Create a `score_event` for the negging player, recording their player_id, team_id, tossup #, neg score, and 0 bonus points
         if request.form.get("neg-score"):
             team_id = db.execute("SELECT team_id FROM players WHERE id = ?", request.form.get("negging-player"))
             if not team_id:
@@ -244,36 +275,45 @@ def game():
                 team_id = team_id[0]["team_id"]
             db.execute("INSERT INTO score_events (game_id, team_id, player_id, tossup_num, score, bonus) VALUES (?, ?, ?, ?, ?, ?)", 
                        session["game_id"], team_id, request.form.get("negging-player"), session["tossup_num"], request.form.get("neg-score"), 0)
-
+        
+        # No `score_event` is created if no player scored or negged: "dead tossup"
+        # Retrieve gamedata necessary to populate the scoresheet
         gamedata = get_gamedata(session["game_id"])
         if gamedata == 404:
             return apology("Game does not exist.", 404)
         elif gamedata == 403:
             return apology("You are not allowed to view this game.", 403)
 
+        # Update current game state
         session["tossup_num"] += 1
         tossup = session["tossup_num"]
         return render_template("game.html", gamedata=gamedata, tossup=tossup)
 
     elif request.method == "GET":
+        # Get the gamedata necessary to populate the scoresheet
         gamedata = get_gamedata(session["game_id"])
         tossup = session["tossup_num"]
         return render_template("game.html", gamedata=gamedata, tossup=tossup)
 
 
+# View user's previous games
 @app.route("/games", methods=["GET", "POST"])
 @login_required
 def games():
     if request.method == "POST":
+        # User must select a game
         if not request.form.get("game"):
             flash("Please select a game.")
             games = db.execute("SELECT * FROM games WHERE user_id = ?", session["user_id"])
             return render_template("games.html", games=games)
         
+        # Submit GET request to view_game with game_id as arg
         return redirect("/view_game?game_id=" + request.form.get("game"))
 
     elif request.method == "GET":
+        # Get all game_ids for current user
         games = db.execute("SELECT * FROM games WHERE user_id = ?", session["user_id"])
+        # Populate the dropdown by naming each game "X vs. Y" where X, Y are the two teams in the game
         for game in games:
             left_team_name = db.execute("SELECT name FROM teams WHERE id = ?", game["left_team_id"])
             right_team_name = db.execute("SELECT name FROM teams WHERE id = ?", game["right_team_id"])
@@ -283,12 +323,16 @@ def games():
         return render_template("games.html", games=games)
 
 
+# View a saved game
 @app.route("/view_game", methods=["GET"])
 @login_required
 def view_game():
     if request.method == "GET":
+        # User must select a game
         if not request.args.get("game_id"):
             return apology("Please select a game.", 404)
+
+        # Get the gamedata necessary to populate the scoresheet
         gamedata = get_gamedata(request.args.get("game_id"))
         if gamedata == 404:
             return apology("Game does not exist.", 404)
@@ -297,6 +341,7 @@ def view_game():
         return render_template("view_game.html", gamedata=gamedata)
 
 
+# Change user's password
 @app.route("/change_password", methods=["GET", "POST"])
 @login_required
 def change_password():
@@ -336,22 +381,26 @@ def change_password():
 
 
 def errorhandler(e):
-    """Handle error"""
+    # Handle errors
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
 
 
+# Helper function that packages the data for the given game into a format
+# used by the Jinja template to render the scoresheet
 def get_gamedata(g_id):
     gamedata = {}
     global db
 
+    # Make sure user is allowed to view this game
     u_id = db.execute("SELECT user_id FROM games WHERE id = ?", g_id)
     if not u_id:
         return 404
     elif u_id[0]["user_id"] != session["user_id"]:
         return 403
 
+    # Get team_ids
     l_id = db.execute("SELECT left_team_id FROM games WHERE id = ?", g_id)
     r_id = db.execute("SELECT right_team_id FROM games WHERE id = ?", g_id)
     if not l_id or not r_id:
@@ -360,16 +409,20 @@ def get_gamedata(g_id):
         l_id = l_id[0]["left_team_id"]
         r_id = r_id[0]["right_team_id"]
 
+    # Get team names
     l_name = db.execute("SELECT name FROM teams WHERE id = ?", l_id)
     r_name = db.execute("SELECT name FROM teams WHERE id = ?", r_id)
     gamedata["left_name"] = l_name[0]["name"]
     gamedata["right_name"] = r_name[0]["name"]
 
+    # Get players
     left_players = db.execute("SELECT * FROM players WHERE team_id = ?", l_id)
     gamedata["left_players"] = left_players
     right_players = db.execute("SELECT * FROM players WHERE team_id = ?", r_id)
     gamedata["right_players"] = right_players
 
+    # Get the scores of the left team, recoring every `score_event` (instance where the player buzzed)
+    # of every player on the team, and computing subtotals and score.
     left_scores = db.execute("SELECT * FROM score_events WHERE game_id = ? AND team_id = ?", g_id, l_id)
     left_tossups = {}
     left_subscore = 0
@@ -391,12 +444,14 @@ def get_gamedata(g_id):
 
     gamedata["left_tossups"] = left_tossups
 
+    # Calculate Points Per Game for each player
     for player in left_players:
         pid = player["id"]
         if not pid in left_ppgs:
             left_ppgs[pid] = 0
     gamedata["left_ppgs"] = left_ppgs
 
+    # Calculate team's Points Per Bonus
     if left_ppb[1] == 0:
         left_ppb = 0
     else:
@@ -405,6 +460,8 @@ def get_gamedata(g_id):
 
     gamedata["left_score"] = left_subscore
     
+    # Get the scores of the right team, recoring every `score_event` (instance where the player buzzed)
+    # of every player on the team, and computing subtotals and score.
     right_scores = db.execute("SELECT * FROM score_events WHERE game_id = ? AND team_id = ?", g_id, r_id)
     right_tossups = {}
     right_subscore = 0
@@ -426,12 +483,14 @@ def get_gamedata(g_id):
 
     gamedata["right_tossups"] = right_tossups
 
+    # Calculate Points Per Game for each player
     for player in right_players:
         pid = player["id"]
         if not pid in right_ppgs:
             right_ppgs[pid] = 0
     gamedata["right_ppgs"] = right_ppgs
 
+    # Calculate team's Points Per Bonus
     if right_ppb[1] == 0:
         right_ppb = 0
     else:
@@ -440,6 +499,7 @@ def get_gamedata(g_id):
 
     gamedata["right_score"] = right_subscore
 
+    # Get how many tossups have passed
     last_tossup = db.execute("SELECT MAX(tossup_num) FROM score_events WHERE game_id = ?", g_id)
     print(last_tossup)
     last_tossup = last_tossup[0]["MAX(tossup_num)"]
